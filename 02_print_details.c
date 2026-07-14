5666b8d1acb1c5a462780adb63a21bab5a9999c7
itishreesolanki <itishreesolanki565@gmail.com>
2026-07-14 20:18:50 +0530

    Add Program 02:Print Details

---
+++ 02_print_details.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	printf("\nName : Itishree Solanki");
+	printf("\nUniversity : Sage University, Indore[Madhya Pradesh]");
+	printf("\nCollege Name : Institute of Advance Computing");
+	printf("\nBranch : Computer Science and Technology ");
+	
+	return 0;
+}
